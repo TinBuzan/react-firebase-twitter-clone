d3d0f93fa3b995763fb8ba23ccaf15b97afa71af
Tin Buzancic <tinbuzan@gmail.com>
2020-01-23 00:54:09 -0500

    finished Scream cards unup until Authentication actually matters.

--- client/src/components/Scream.js
+++ client/src/components/Scream.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 // MUI imports
+import withStyles from '@material-ui/core/styles/withStyles';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
@@ -17,7 +19,8 @@ const styles = {
     minWidth: 200
   },
   content: {
-    padding: 25
+    padding: 25,
+    objectFit: 'cover'
   }
 }
 
@@ -25,13 +28,14 @@ export class Scream extends Component {
   render() {
     // const classes = this.props.classes
     const { classes, scream }  = this.props
-
+    dayjs.extend(relativeTime);
+    
     return (
       <Card className={classes.card}>
           <CardMedia image={scream.userImage}  title="Profile Image" className={classes.image}/>
           <CardContent className={classes.content}>
             <Typography variant="h5" component={Link} to={`/users/${scream.userHandle}`} color="primary">{scream.userHandle}</Typography>
-            <Typography variant="body2" color="textSecondary">{scream.createdAt}</Typography>
+            <Typography variant="body2" color="textSecondary">{dayjs(scream.createdAt).fromNow()}</Typography>
             <Typography variant="body1">{scream.body}</Typography>
           </CardContent>
       </Card>
